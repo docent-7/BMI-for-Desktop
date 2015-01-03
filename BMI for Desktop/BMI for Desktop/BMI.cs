5c4eb11a26ec274108690435041fc87fdfa5ccfd
Marcin Szałaj <marcinszalaj@outlook.com>
2015-01-03 19:11:39 +0100

    dodanie klasy do projektu
    BMI for Desktop Application

---
+++ BMI for Desktop/BMI for Desktop/BMI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI_for_Desktop
+{
+    class BMI
+    {
+        public float masaOsoby { get; set; }
+        public float wzrostOsoby { get; set; }
+
+        private float masaCzlowieka;
+        private float wzrostCzlowieka;
+
+        private float BMI;
+
+        public float obliczBMI(float masa, float wzrost)
+        {
+            BMI = masa / (wzrost * wzrost);
+
+            return BMI;
+        }
+    }
+}
